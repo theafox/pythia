817f1719187ea29939e5d4458b874e02059b32db
Thea K. <thea.kaufmann@proton.me>
2024-06-02 13:57:09 +0200

    docs(linter): add documentation to all `utils`

--- python/probros/linter/rules/utils.py
+++ python/probros/linter/rules/utils.py
@@ -2,6 +2,20 @@ import ast
 
 
 def is_function_called(node: ast.AST, name: str) -> bool:
+    """Check if the given node is a function call with the given name.
+
+    This checks for both, direct function calls, i.e. `myname(...)`, and
+    through attributes, i.e. `mypackage.myname(...)`.
+
+    Args:
+        node: The AST node to check.
+        name: The name of the function to check for.
+
+    Returns:
+        bool: `True` if the node is a call to that specified function, `False`
+        otherwise.
+    """
+
     match node:
         case ast.Call(
             func=(ast.Name(id=called) | ast.Attribute(attr=called))
@@ -12,15 +26,34 @@ def is_function_called(node: ast.AST, name: str) -> bool:
 
 
 class Address:
+    """A utility class for working with address-representations.
+
+    This class is designed to be static encapsulation of address-related
+    features and should not be instantiated.
+    """
 
     # NOTE: extract this dynamically to future-proof for changes?
     _ADDRESS_CALL = "IndexedAddress"
 
     def __new__(cls):
+        """Prevent instantiation of this class.
+
+        Raises:
+            RuntimeError: Always raised to prevent instantiation.
+        """
         raise RuntimeError(f"{cls} is a static class.")
 
     @classmethod
     def is_address(cls, node: ast.AST) -> bool:
+        """Check whether a given node is an address.
+
+        Args:
+            node: The AST node check
+
+        Returns:
+            `True` if the node represents an address, `False` otherwise.
+        """
+
         match node:
             case ast.Constant(value=str()):
                 return True
@@ -33,10 +66,20 @@ class Address:
 
     @classmethod
     def representation(cls) -> str:
+        """Get the string representation of the address type.
+
+        Returns:
+            The string representation of the address type.
+        """
         return f"str | {cls._ADDRESS_CALL}(...)"
 
 
 class Distribution:
+    """A utility class for working with distribution-representations.
+
+    This class is designed to be static encapsulation of address-related
+    features and should not be instantiated.
+    """
 
     # NOTE: extract this dynamically to future-proof for changes?
     _DISTRIBUTIONS = [
@@ -64,10 +107,24 @@ class Distribution:
     ]
 
     def __new__(cls):
+        """Prevent instantiation of this class.
+
+        Raises:
+            RuntimeError: Always raised to prevent instantiation.
+        """
         raise RuntimeError(f"{cls} is a static class.")
 
     @classmethod
     def is_distribution(cls, node: ast.AST) -> bool:
+        """Check whether a given node is an distribution.
+
+        Args:
+            node: The AST node check
+
+        Returns:
+            `True` if the node represents a distribution, `False` otherwise.
+        """
+
         return any(
             is_function_called(node, distribution)
             for distribution in cls._DISTRIBUTIONS
@@ -75,4 +132,9 @@ class Distribution:
 
     @classmethod
     def representation(cls) -> str:
+        """Get the string representation of the distribution type.
+
+        Returns:
+            The string representation of the distribution type.
+        """
         return "distribution"
